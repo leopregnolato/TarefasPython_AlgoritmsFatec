1c0088c133d52daad0ce7c8567c6443b3d8faef8
Leonardo Pregnolato <you@example.com>
2021-08-25 19:16:27 -0300

    Resolução do uri1037

--- posAula4/uri1037.py
+++ posAula4/uri1037.py
@@ -15,3 +15,17 @@ O arquivo de entrada contém um número com ponto flutuante qualquer.
 Saída
 A saída deve ser uma mensagem conforme exemplo abaixo.
 '''
+
+number = float(input())
+
+if number >= 0 and number <= 25:
+    print("Intervalo [0,25]")
+elif number > 25 and number <= 50:
+    print("Intervalo (25,50]")
+elif number > 50 and number <= 75:
+    print("Intervalo (50,75]")
+elif number > 75 and number <= 100:
+    print("Intervalo (75,100]")
+else:
+    print("Fora de intervalo")
+
