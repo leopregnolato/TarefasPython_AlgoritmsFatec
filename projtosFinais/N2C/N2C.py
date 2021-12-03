fc547868790c4d5372600e4aa272d5fce145db19
Leonardo A. Pregnolato <78579109+leopregnolato@users.noreply.github.com>
2021-12-02 21:03:45 -0300

    revisão e correção dos bugs exercício N2C

--- projtosFinais/N2C/N2C.py
+++ projtosFinais/N2C/N2C.py
@@ -11,7 +11,7 @@ def CalcAliqINSS(SalBruto):
     elif S >= 2919.73 and S <= 5839.45:
         AliqINSS = 0.11
     elif S >= 5839.46:
-        AliqINSS = 1
+        AliqINSS = 0
     SalBruto = S
     return SalBruto, AliqINSS
   
@@ -24,16 +24,16 @@ def CalcAliqIR(SalBruto, ValINSS):
     if base <= 1903.98:        
         AliqIR = 0
         deducao = 0
-    elif base >= 1903.99 and S <= 2826.65:       
+    elif base >= 1903.99 and base <= 2826.65:       
         AliqIR = 0.075
         deducao = 142.8
-    elif base >= 2826.66 and S <= 3751.05:
+    elif base >= 2826.66 and base <= 3751.05:
         AliqIR = 0.15
         deducao = 354.80
-    elif base >= 3751.06 and S <= 4664.68:
+    elif base >= 3751.06 and base <= 4664.68:
         AliqIR = 0.225
         deducao = 636.13
-    elif S >= 4664.69:
+    elif base >= 4664.69:
         AliqIR = 0.275
         deducao = 869.36
     return base, AliqIR, deducao
@@ -57,7 +57,7 @@ ValIR = 0
 SalLiquido = 0
 base = 0
 
-Arq = open("testepdf.txt", "r")
+Arq = open("SALARIO.txt", "r")
 
 Salario = []
 
@@ -89,12 +89,12 @@ while i < len(Salario):
     S = float(S)
     PBruto, PAliqINSS = CalcAliqINSS(S)
     PValINSS = CalcValINSS(PBruto, PAliqINSS)
-    PBaseIR, PAliqIr, PDeducao = CalcAliqIR(PBruto, PValINSS)
-    PValIR = CalcValIR(PBruto, PValINSS, PAliqIr, PDeducao) 
+    PBaseIR, PAliqIR, PDeducao = CalcAliqIR(PBruto, PValINSS)
+    PValIR = CalcValIR(PBruto, PValINSS, PAliqIR, PDeducao) 
     PSalLiquido = CalcSalLiquido(PBruto, PValINSS, PValIR)
     PAliqINSS = PAliqINSS * 100
-    PAliqIr = PAliqIr * 100    
-    arquivo.write(f"{PBruto: >10.2f}{PAliqINSS: >15.1f}{PValINSS: >15.2f}{PBaseIR: >15.2f}{PAliqIr: >15.1f}{PValIR: >15.2f}{PSalLiquido: >15.2f}\n")
+    PAliqIR = PAliqIR * 100    
+    arquivo.write(f"{PBruto: >10.2f}{PAliqINSS: >15.1f}{PValINSS: >15.2f}{PBaseIR: >15.2f}{PAliqIR: >15.1f}{PValIR: >15.2f}{PSalLiquido: >15.2f}\n")
     i = i + 1
     
 arquivo.close()
